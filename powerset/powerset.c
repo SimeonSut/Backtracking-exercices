17c48bcc4eccd3d5651b9839c6495769e5abcc88
Simeon Sutarmin <ssutarmi@c3r3s3.42lausanne.ch>
2026-08-03 16:24:01 +0200

    asfas

--- powerset/powerset.c
+++ powerset/powerset.c
@@ -6,7 +6,7 @@
 /*   By: ssutarmi <ssutarmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 16:18:14 by ssutarmi          #+#    #+#             */
-/*   Updated: 2026/08/02 21:37:43 by ssutarmi         ###   ########.fr       */
+/*   Updated: 2026/08/03 16:13:40 by ssutarmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,41 +22,10 @@ void	print_solution(int *solution_set, int solution_len)
 	printf("%d\n", *solution_set);
 }
 
-int		check_condition(t_set *node, int *subset)
-{
-	int	i;
-
-	i = 0;
-	while (i < node->set_len)
-	{
-		if (subset[i] == 1)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
 void	powerset(t_set *node, int start, int *subset, int subset_sum)
 {
-	int	i;
-
-	i = start;
-	if (subset[start] == 1)
-		subset[start] == 0;
-	while (i < node->set_len)
-	{
-		if (start == 0 || (start != 0 && i != start))
-			subset[i] = 1;
-		if (subset[i] == 1)
-			subset_sum += node->set[i];
-		else
-			subset_sum -= node->set[i];
-		if (subset_sum == node)
-			;//print_solution
-		i++;
-	}
-	if (check_condition(node, subset) == 1)
-		powerset(node, --i, subset, subset_sum);
+	if (subset[node->set_len - 1] == 1)
+		;//backtracking
 }
 
 int main(int argc, char **argv)
