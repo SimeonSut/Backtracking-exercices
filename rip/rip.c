8612b6c0347cb3ffaad3abd5e0d55167f0ab1a79
Simeon Sutarmin <ssutarmi@c3r2s2.42lausanne.ch>
2026-08-06 16:57:31 +0200

    work on rip and checking logic

--- rip/rip.c
+++ rip/rip.c
@@ -6,14 +6,37 @@
 /*   By: ssutarmi <ssutarmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 14:57:14 by ssutarmi          #+#    #+#             */
-/*   Updated: 2026/08/05 21:33:52 by ssutarmi         ###   ########.fr       */
+/*   Updated: 2026/08/06 16:56:57 by ssutarmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int		check_validity(char *str)
+{
+	int	i;
+	int	quote;
+
+	i = 0;
+	quote = 0;
+	if (str[i] == '(')
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '(')
+			quote++;
+		else if (str[i] == ')')
+			;
+	}
+	return (quote);
+}
+
+void	rip(char *str)
+{
+}
+
+int 	main(int argc, char **argv)
 {
 	char	*str;
 
