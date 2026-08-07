fbbd72391d8c747046dca72a0750a803cc9918c6
Simeon Sutarmin <ssutarmi@c3r6s1.42lausanne.ch>
2026-08-07 21:07:16 +0200

    creation of check validity function of rip, work on main recursion underway

--- rip/rip.c
+++ rip/rip.c
@@ -6,34 +6,58 @@
 /*   By: ssutarmi <ssutarmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 14:57:14 by ssutarmi          #+#    #+#             */
-/*   Updated: 2026/08/06 16:56:57 by ssutarmi         ###   ########.fr       */
+/*   Updated: 2026/08/07 21:06:28 by ssutarmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-int		check_validity(char *str)
+int		check_validity(char *str, int ignore)
 {
 	int	i;
-	int	quote;
+	int	count;
 
 	i = 0;
-	quote = 0;
-	if (str[i] == '(')
-		return (0);
+	count = 0;
 	while (str[i])
 	{
 		if (str[i] == '(')
-			quote++;
-		else if (str[i] == ')')
-			;
+			count++;
+		if (str[i] == ')')
+			count--;
+		if (count < 0)
+			return (1);
+		i++;
 	}
-	return (quote);
+	if (count > 0)
+		return (1);
+	return (0);
 }
 
-void	rip(char *str)
+void	rip(char *str, int prev_space, int char_input)
 {
+	int		i;
+	char	tmp;
+
+	i = 0;
+	while (str[i])
+	{
+		if (check_validity(str, i) == 0)
+		{
+			;//print_solution
+			return ;
+		}
+		i++;
+		if ((!str[i] || i == prev_space) && i > 0)
+		{
+			str[i] = char_input;
+			i--;
+			tmp = str[i];
+			str[i] = ' ';
+			rip(str, i, tmp);
+		}
+	}
 }
 
 int 	main(int argc, char **argv)
@@ -43,5 +67,6 @@ int 	main(int argc, char **argv)
 	if (argc != 2)
 		return (1);
 	str = argv[1];
+	rip(str);
 	return (0);
 }
